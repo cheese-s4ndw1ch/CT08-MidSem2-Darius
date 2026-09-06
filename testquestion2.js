0c266f2e4acb37bde7875bd65ce2ca544af12d93
toofwess <darius.lim.rx@gmail.com>
2026-09-06 03:44:26 +0000

    Sep 6, 2026, 3:44 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,8 +5,8 @@ function setup() {
   new Canvas(600, 400);
   world.gravity.y = 10;
 
-  
-  box = new Sprite(300, 200, 50, 50);
+
+  box = new Sprite(30, 30, 50, 50);
   ground = new Sprite(300, 350, 600, 100);
 
 }
