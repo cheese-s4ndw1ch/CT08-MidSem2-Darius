a2857b5bdf92e544c85b24d81c1f5b1b24a9a8fd
toofwess <darius.lim.rx@gmail.com>
2026-09-06 03:27:53 +0000

    Sep 6, 2026, 3:27 AM

--- testquestion1.js
+++ testquestion1.js
@@ -2,18 +2,15 @@ let player;
 let floor;
 
 function setup() {
-
   new Canvas(600, 400);
 
   world.gravity.y = 10;
 
-  
   player = new Sprite(300, 100, 30, 30);
   player.color = 'blue';
-  player.collider = 'dynamic'
+  player.collider = 'dynamic';
 
-  
-  floor = new Sprite(1200, 390, 2400, 20);
+  floor = new Sprite(width / 2, height - 10, 2400, 20);
   floor.collider = 'static';
   floor.color = 'green';
 }
